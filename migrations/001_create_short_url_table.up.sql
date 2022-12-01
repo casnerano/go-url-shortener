6983fbb9f7903079bab89994bcb96cdbdaa6585f
Arutyunyan A <casnerano@mail.ru>
2022-12-01 19:12:18 +0300

    added migration file

--- migrations/001_create_short_url_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "short_url" (
-  "id" serial NOT NULL,
-  PRIMARY KEY ("id"),
-  "code" character(255) NOT NULL,
-  "original" text NOT NULL,
-  "created_at" timestamp NOT NULL,
-  "lifetime" integer NULL
-);
