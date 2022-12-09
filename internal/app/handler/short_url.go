2554c038271ea33e10149c07ae3cbb539d32270c
Arutyunyan A <casnerano@mail.ru>
2022-12-09 17:24:57 +0300

    bugfix

--- internal/app/handler/short_url.go
+++ internal/app/handler/short_url.go
@@ -1,119 +1,117 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
 
-	"github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5"
 
-	"github.com/casnerano/go-url-shortener/internal/app/service"
+    "github.com/casnerano/go-url-shortener/internal/app/service"
 )
 
 type ShortURL struct {
-	urlService *service.URL
+    urlService *service.URL
 }
 
 func NewShortURL(urlService *service.URL) *ShortURL {
-	return &ShortURL{urlService}
+    return &ShortURL{urlService}
 }
 
 func (s *ShortURL) GetOriginalURL(w http.ResponseWriter, r *http.Request) {
-	shortCode := chi.URLParam(r, "shortCode")
+    shortCode := chi.URLParam(r, "shortCode")
 
-	if shortCode == "" {
-		http.Error(w, "bad request", http.StatusBadRequest)
-		return
-	}
+    if shortCode == "" {
+        http.Error(w, "bad request", http.StatusBadRequest)
+        return
+    }
 
-	shortURL, err := s.urlService.GetByCode(shortCode)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
+    shortURL, err := s.urlService.GetByCode(shortCode)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
 
-	http.Redirect(w, r, shortURL.Original, http.StatusTemporaryRedirect)
+    http.Redirect(w, r, shortURL.Original, http.StatusTemporaryRedirect)
 }
 
 func (s *ShortURL) PostText(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
+    body, err := io.ReadAll(r.Body)
+    defer r.Body.Close()
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
-	urlOriginal := string(body)
+    urlOriginal := string(body)
 
-	if urlOriginal == "" {
-		http.Error(w, "bad request", http.StatusBadRequest)
-		return
-	}
+    if urlOriginal == "" {
+        http.Error(w, "bad request", http.StatusBadRequest)
+        return
+    }
 
-	shortURLModel, err := s.urlService.Create(urlOriginal)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    shortURLModel, err := s.urlService.Create(urlOriginal)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprint(w, s.buildAbsoluteShortURL(r, shortURLModel.Code))
+    w.WriteHeader(http.StatusCreated)
+    fmt.Fprint(w, s.buildAbsoluteShortURL(r, shortURLModel.Code))
 }
 
 func (s *ShortURL) PostJSON(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
+    body, err := io.ReadAll(r.Body)
+    defer r.Body.Close()
 
-	w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Content-Type", "application/json")
 
-	if err != nil {
-		http.Error(w, s.createErrJSON(err.Error()), http.StatusInternalServerError)
-		return
-	}
+    if err != nil {
+        http.Error(w, s.createErrJSON(err.Error()), http.StatusInternalServerError)
+        return
+    }
 
-	bodyObj := struct {
-		URL string `json:"url"`
-	}{}
-	err = json.Unmarshal(body, &bodyObj)
+    bodyObj := struct {
+        URL string `json:"url"`
+    }{}
+    err = json.Unmarshal(body, &bodyObj)
 
-	fmt.Println(bodyObj)
+    if err != nil || bodyObj.URL == "" {
+        http.Error(w, s.createErrJSON("bad request"), http.StatusBadRequest)
+        return
+    }
 
-	if err != nil || bodyObj.URL == "" {
-		http.Error(w, s.createErrJSON("bad request"), http.StatusBadRequest)
-		return
-	}
+    shortURLModel, err := s.urlService.Create(bodyObj.URL)
+    if err != nil {
+        http.Error(w, s.createErrJSON(err.Error()), http.StatusInternalServerError)
+        return
+    }
 
-	shortURLModel, err := s.urlService.Create(bodyObj.URL)
-	if err != nil {
-		http.Error(w, s.createErrJSON(err.Error()), http.StatusInternalServerError)
-		return
-	}
+    w.WriteHeader(http.StatusCreated)
 
-	w.WriteHeader(http.StatusCreated)
+    response := struct {
+        Result string `json:"result"`
+    }{
+        s.buildAbsoluteShortURL(r, shortURLModel.Code),
+    }
 
-	response := struct {
-		Result string `json:"result"`
-	}{
-		s.buildAbsoluteShortURL(r, shortURLModel.Code),
-	}
-
-	rb, _ := json.Marshal(response)
-	fmt.Fprint(w, string(rb))
+    rb, _ := json.Marshal(response)
+    fmt.Fprint(w, string(rb))
 }
 
 func (s *ShortURL) buildAbsoluteShortURL(r *http.Request, shortCode string) string {
-	scheme := "http"
-	if r.TLS != nil {
-		scheme = "https"
-	}
-	return fmt.Sprintf("%s://%s/%s", scheme, r.Host, shortCode)
+    scheme := "http"
+    if r.TLS != nil {
+        scheme = "https"
+    }
+    return fmt.Sprintf("%s://%s/%s", scheme, r.Host, shortCode)
 }
 
 func (s *ShortURL) createErrJSON(err string) string {
-	result, _ := json.Marshal(struct {
-		Error string `json:"error"`
-	}{err})
-	return string(result)
+    result, _ := json.Marshal(struct {
+        Error string `json:"error"`
+    }{err})
+    return string(result)
 }
--- internal/app/server/application.go
+++ internal/app/server/application.go
@@ -1,111 +1,111 @@
 package server
 
 import (
-    "context"
-    "flag"
-    "fmt"
-    "log"
-    "net/http"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/go-chi/chi/v5"
-    "github.com/jackc/pgx/v5"
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
 
-    "github.com/casnerano/go-url-shortener/internal/app/config"
-    "github.com/casnerano/go-url-shortener/internal/app/handler"
-    "github.com/casnerano/go-url-shortener/internal/app/repository"
-    "github.com/casnerano/go-url-shortener/internal/app/repository/memstore"
-    "github.com/casnerano/go-url-shortener/internal/app/repository/sqlstore"
-    "github.com/casnerano/go-url-shortener/internal/app/service"
-    "github.com/casnerano/go-url-shortener/internal/app/service/hasher"
+	"github.com/casnerano/go-url-shortener/internal/app/config"
+	"github.com/casnerano/go-url-shortener/internal/app/handler"
+	"github.com/casnerano/go-url-shortener/internal/app/repository"
+	"github.com/casnerano/go-url-shortener/internal/app/repository/memstore"
+	"github.com/casnerano/go-url-shortener/internal/app/repository/sqlstore"
+	"github.com/casnerano/go-url-shortener/internal/app/service"
+	"github.com/casnerano/go-url-shortener/internal/app/service/hasher"
 )
 
 type Application struct {
-    Config *config.Config
-    Store  repository.Store
-    router *chi.Mux
+	Config *config.Config
+	Store  repository.Store
+	router *chi.Mux
 }
 
 func NewApplication() *Application {
-    app := &Application{}
-    app.init()
-    return app
+	app := &Application{}
+	app.init()
+	return app
 }
 
 func (app *Application) init() {
-    app.initConfig()
-    app.initRepositoryStore()
-    app.initRouter()
-    app.initRoutes()
+	app.initConfig()
+	app.initRepositoryStore()
+	app.initRouter()
+	app.initRoutes()
 }
 
 // Запуск сервера
 func (app *Application) RunServer() error {
-    fmt.Printf("Server started: %s\n", app.Config.ServerAddr)
-    fmt.Printf("Use storage is %s\n", app.Config.Storage.Type)
-    return http.ListenAndServe("127.0.0.1:8080", app.router)
+	fmt.Printf("Server started: %s\n", app.Config.ServerAddr)
+	fmt.Printf("Use storage is %s\n", app.Config.Storage.Type)
+	return http.ListenAndServe(app.Config.ServerAddr, app.router)
 }
 
 // Путь к файлу конфигурации из параметров запуска приложения
 func (app *Application) extractAppConfigName() string {
-    confName := flag.String("config", "", "app configuration filename")
-    flag.Parse()
-    return *confName
+	confName := flag.String("config", "", "app configuration filename")
+	flag.Parse()
+	return *confName
 }
 
 // Инициализация конфигурации
 func (app *Application) initConfig() {
-    app.Config = config.New()
+	app.Config = config.New()
 
-    if cfgName := app.extractAppConfigName(); cfgName != "" {
-        if err := config.Unmarshal(cfgName, app.Config); err != nil {
-            log.Fatalf("failed to read file %s", cfgName)
-        }
-    }
+	if cfgName := app.extractAppConfigName(); cfgName != "" {
+		if err := config.Unmarshal(cfgName, app.Config); err != nil {
+			log.Fatalf("failed to read file %s", cfgName)
+		}
+	}
 
-    _ = config.OverrideFromENV(app.Config)
+	_ = config.OverrideFromENV(app.Config)
 }
 
 func (app *Application) initRouter() {
-    app.router = chi.NewRouter()
+	app.router = chi.NewRouter()
 }
 
 // Группа репозиториев для хранилища
 // По умолчанию - хранилище в озу - memstore.Store
 func (app *Application) initRepositoryStore() {
-    switch app.Config.Storage.Type {
-    case config.StorageTypeDatabase:
-        dsn := app.Config.Storage.DSN
-        pgxConn, err := pgx.Connect(context.TODO(), dsn)
-        if err != nil {
-            log.Fatalf("Failed to connect to the database using dsn \"%s\"", dsn)
-        }
-        app.Store = sqlstore.NewStore(pgxConn)
-    default:
-        app.Store = memstore.NewStore()
-    }
+	switch app.Config.Storage.Type {
+	case config.StorageTypeDatabase:
+		dsn := app.Config.Storage.DSN
+		pgxConn, err := pgx.Connect(context.TODO(), dsn)
+		if err != nil {
+			log.Fatalf("Failed to connect to the database using dsn \"%s\"", dsn)
+		}
+		app.Store = sqlstore.NewStore(pgxConn)
+	default:
+		app.Store = memstore.NewStore()
+	}
 }
 
 // Инициализация роутов
 func (app *Application) initRoutes() {
-    shortURL := app.getShortURLHandlerGroup()
+	shortURL := app.getShortURLHandlerGroup()
 
-    app.router.Get("/{shortCode}", shortURL.GetOriginalURL)
-    app.router.Post("/", shortURL.PostText)
+	app.router.Get("/{shortCode}", shortURL.GetOriginalURL)
+	app.router.Post("/", shortURL.PostText)
 
-    app.router.Route("/api", func(r chi.Router) {
-        r.Post("/shorten", shortURL.PostJSON)
-    })
+	app.router.Route("/api", func(r chi.Router) {
+		r.Post("/shorten", shortURL.PostJSON)
+	})
 }
 
 // Сервис для сокращения URL
 func (app *Application) getURLHashService() (h hasher.Hash) {
-    h, _ = hasher.NewRandom(5, 10)
-    return
+	h, _ = hasher.NewRandom(5, 10)
+	return
 }
 
 // Группа обработчиков для сокращения URL
 func (app *Application) getShortURLHandlerGroup() *handler.ShortURL {
-    URLRepository := app.Store.URL()
-    hashService := app.getURLHashService()
-    return handler.NewShortURL(service.NewURL(URLRepository, hashService))
+	URLRepository := app.Store.URL()
+	hashService := app.getURLHashService()
+	return handler.NewShortURL(service.NewURL(URLRepository, hashService))
 }
