400613de23f58a39a12ce698ec458e6bea29c443
Arutyunyan A <46101935+casnerano@users.noreply.github.com>
2022-12-25 10:10:59 +0300

    Iter8 (#20)
    * refactoring
    
    * gix editorconfig

--- internal/app/model/short_url_test.go
+++ internal/app/model/short_url_test.go
@@ -9,12 +9,14 @@ import (
 
 func TestNewShortURL(t *testing.T) {
 	shortURLOne := ShortURL{
+		1,
 		"short#1",
 		"large#1",
 		time.Now(),
 	}
 
 	got := NewShortURL(shortURLOne.Code, shortURLOne.Original)
+	got.ID = shortURLOne.ID
 	got.CreatedAt = shortURLOne.CreatedAt
 
 	assert.Equal(t, shortURLOne, *got)
