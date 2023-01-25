e2d3bac54c75ee98e746cf34ef8ecd458561779c
Arutyunyan A <casnerano@mail.ru>
2023-01-25 14:32:42 +0300

    log

--- internal/app/handler/short_url.go
+++ internal/app/handler/short_url.go
@@ -254,7 +254,9 @@ func (s *ShortURL) DeleteBatchJSON(w http.ResponseWriter, r *http.Request) {
 	}
 
 	go func() {
+		fmt.Println("start delete part")
 		_ = s.urlService.DeleteBatch(codes, uuid)
+		fmt.Println("finish delete part")
 	}()
 
 	w.WriteHeader(http.StatusAccepted)
