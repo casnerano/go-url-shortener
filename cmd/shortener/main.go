0559a7d72df15fecb07e26bf1111082329c33260
Arutyunyan A <casnerano@mail.ru>
2023-03-21 21:31:03 +0300

    added vars

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/casnerano/go-url-shortener/internal/app/server"
 	"github.com/casnerano/go-url-shortener/internal/app/service/cleaner"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+
+	fmt.Printf(
+		"Build version: %s\nBuild date: %s\nBuild commit: %s\n",
+		buildVersion,
+		buildDate,
+		buildCommit,
+	)
+
 	app := server.NewApplication()
 	defer app.Shutdown()
 
